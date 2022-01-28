852cf7dc043a0215c74296a961eaf225e4ab3d4e
Cameron Aziz <cameronaziz@me.com>
2022-01-27 22:38:05 -0800

    Initial Commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div>
+        <h1>My name is Dior and I work for Ticket Fanatic</h1>
+      </div>
+    </div>
+  );
+}
